726def00e5ea4ff1c87d2473ec397a02f7ffbeed
Peter Sharma <peterksharma@gmail.com>
2023-04-19 19:10:27 -0500

    final updates before submit

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ $(function () {
     var startHour = 9; // 9AM
     var endHour = 18; // 6PM
 
+    //for loop to add classes
     for (var hour = startHour; hour < endHour; hour++) {
       var timeBlock = $('<div>').addClass('row time-block').attr('id', 'hour-' + hour);
       var hourLabel = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(hourToAmPm(hour));
       var textarea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+      //getting the text from local storage
       var savedText = localStorage.getItem('hour-' + hour)
       var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
       var saveIcon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
